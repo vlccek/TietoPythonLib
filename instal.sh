4cf7dad372e39b7e6b5b59efbdc80d79d1eca562
Jakub <jakub@jevlk.cz>
2021-07-29 14:46:01 +0200

    Improve isntalation script

--- instal.sh
+++ instal.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-apt-get update  # To get the latest package lists
 apt-get install git -y
 apt install python3-pip
+ sudo apt-get install build-essential libssl-dev libffi-dev python3-dev cargo
+pip3 install -r requirements.txt
+pip3 install setuptools
+
 #etc.
