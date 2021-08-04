f91cb69e75f4b4c54531b03a14eb8ae97d89c65e
Jakub Vlk <jakub@jevlk.cz>
2021-08-04 11:37:27 +0200

    Update install script

--- instal.sh
+++ instal.sh
@@ -2,7 +2,7 @@
 apt-get install git -y
 apt install python3-pip
 sudo apt-get -y install build-essential libssl-dev libffi-dev python3-dev cargo
-pip3 -y install setuptools
-pip3 -y install -r requirements.txt
+yes | pip3 install setuptools
+yes | pip3 install setuptools_rust
+yes | pip3 install -r requirements.txt
 
-#etc.
