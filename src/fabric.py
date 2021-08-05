2998f119302d7b6da46c8e5ac87515b7012bca25
Jakub Vlk <jakub@jevlk.cz>
2021-08-05 11:52:28 +0200

    Add logger wraper

--- src/fabric.py
+++ src/fabric.py
@@ -110,3 +110,24 @@ class Fabric:
     def __del__(self):
         self.__connection.close()
         print("removing obj")
+
+
+def logger_wraps(*, entry=True, exit=True, level="DEBUG"):
+
+    def wrapper(func):
+        name = func.__name__
+
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs):
+            logger_ = logger.opt(depth=1)
+            if entry:
+                logger_.log(
+                    level, "Entering '{}' (args={}, kwargs={})", name, args, kwargs)
+            result = func(*args, **kwargs)
+            if exit:
+                logger_.log(level, "Exiting '{}' (result={})", name, result)
+            return result
+
+        return wrapped
+
+    return wrapper
