94cd5a7230cef3fb0666d37ac5d2400457962b6f
Jakub Vlk <jakub@jevlk.cz>
2021-08-05 10:45:33 +0200

    edit die message

--- src/fabric.py
+++ src/fabric.py
@@ -90,20 +90,12 @@ class Fabric:
 
     def send_command(self, command:str):
         stdin, stdout, stderr = self.__connection.exec_command(command)
-<<<<<<< HEAD
         # logger.info(f"Command was send. stdout {stdout.read()} ")
-=======
-        logger.info(f"Command was send. stdout {stdout.read()} ")
->>>>>>> 28de2ce93203a12ac96c128d606303ab7b3b6ee9
         # logger.trace(f"Command was send. stdout {stdout.read()} ")
         if not stderr == "":
             logger.error(f"Command was send. stderr {stderr.read()}")
 
-<<<<<<< HEAD
         # logger.info(f"Command was send. stdout {stdout.read()} ")
-=======
-        logger.info(f"Command was send. stdout {stdout.read()} ")
->>>>>>> 28de2ce93203a12ac96c128d606303ab7b3b6ee9
         return stdin, stdout, stderr
 
 
@@ -115,4 +107,4 @@ class Fabric:
 
     def __del__(self):
         self.__connection.close()
-        print("die")
+        print("removing obj")
