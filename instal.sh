565581c36a0184b07ae2bfb82789b547afeb5eef
Jakub Vlk <jakub@jevlk.cz>
2021-08-25 14:29:12 +0200

    add upgrading pip in instalation

--- instal.sh
+++ instal.sh
@@ -3,6 +3,7 @@ apt-get install git -y
 apt install python3-pip
 apt-get -y install python3-pip
 sudo apt-get -y install build-essential libssl-dev libffi-dev python3-dev cargo
+yes | pip3 install --upgrade pip
 yes | pip3 install setuptools
 yes | pip3 install setuptools_rust
 yes | pip3 install loguru 
