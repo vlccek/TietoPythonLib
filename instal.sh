a9fcfeb10f95be1250f109f19d6fa858f4eba88c
Jakub Vlk <jakub@jevlk.cz>
2021-08-27 15:37:16 +0200

    update install script

--- instal.sh
+++ instal.sh
@@ -4,8 +4,8 @@ apt install python3-pip
 apt-get -y install python3-pip
 sudo apt-get -y install build-essential libssl-dev libffi-dev python3-dev cargo
 yes | pip3 install --upgrade pip
-yes | pip3 install -u setuptools
-yes | pip3 install -u setuptools_rust
-yes | pip3 install -u loguru 
+yes | pip3 install setuptools
+yes | pip3 install setuptools_rust
+yes | pip3 install loguru 
 yes | pip3 install -r requirements.txt
 
