a434fd8dd0315dd3d41dc94422325850155d3057
pitris90 <petr.balnar@seznam.cz>
2021-09-27 12:13:51 +0200

    diags beta 0.9.0

--- src/diags.py
+++ src/diags.py
@@ -4,14 +4,16 @@ import datetime
 from download import open_connection
 from tests import settings
 
+
 def download_diags(
-        self,
-        hostname: str = settings.hostname,
-        port: int = settings.port,
-        timeout: int = 60,
-        keepalive: int = 60,
-    ) -> None:
-    network = open_connection("network-admin", settings.password, hostname, port, timeout, keepalive)
+    self,
+    hostname: str = settings.hostname,
+    port: int = settings.port,
+    timeout: int = 60,
+    keepalive: int = 60,
+) -> None:
+    network = open_connection(
+        "network-admin", settings.password, hostname, port, timeout, keepalive)
 
     network.exec_command(f"admin-sftp-modify enable")
     if "Diagnostics info saved." not in send_command_with_input(network, "save-diags"):
@@ -24,19 +26,31 @@ def download_diags(
     logger.info(f"export-diags ran properly")
     network.close()
     logger.info(f"Closed connection as network-admin")
-    admin = open_connection("admin", settings.password, hostname, port, timeout, keepalive)
+
+    admin = open_connection("admin", settings.password,
+                            hostname, port, timeout, keepalive)
+
+    latest = 0
+    latestfile = None
 
     sftp = admin.open_sftp()
-    sftp.get(local_path, remote_path)
+    sftp.chdir('/sftp/export')
+    for fileattr in sftp.listdir_attr():
+        if fileattr.filename.startswith('core.nvOSd') and fileattr.st_mtime > latest:
+            latest = fileattr.st_mtime
+            latestfile = fileattr.filename
+
+    if latestfile is not None:
+        sftp.get(latestfile, latestfile)
+
     sftp.close()
-    connection_network.close()
+    admin.close()
+
 
 def send_command_with_input(connection, command):
-    stdin, stdout, stderr = connection.exec_command(command, get_pty = True)
+    stdin, stdout, stderr = connection.exec_command(command, get_pty=True)
     stdin.write("/n")
     stdin.flush()
     stdin.write(settings.password + "/n")
     stdin.flush()
     return str(stdout.read())
-    
-    
